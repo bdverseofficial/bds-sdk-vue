c61a54f996d48e9024cbc1c1f029e0444e943756
Support <support@bdverse.com>
2020-05-06 11:33:59 +0200

    Fix licence

--- src/models/Lic.ts
+++ src/models/Lic.ts
@@ -2,8 +2,9 @@ import { BdsEntity } from './BdsEntity';
 import { Reference } from './Reference';
 
 export interface Licence extends BdsEntity {
-    tags: string[];
-    expirationDate: Date;
-    owner: Reference;
-    licenceKey: string;
+    tags?: string[];
+    expirationDate?: Date;
+    owner?: Reference;
+    licenceKey?: string;
+    status?: string;
 }
