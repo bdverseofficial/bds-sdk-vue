d9b2fc866067cde611c8e5c785d00d5055803eb7
Support <support@bdverse.com>
2020-05-12 14:39:14 +0200

    Add some Crm definitino

--- src/models/Crm.ts
+++ src/models/Crm.ts
@@ -1,7 +1,11 @@
 import { Phone, User, Address } from './User';
+import { Reference } from './Reference';
 
 export interface Personal {
     mobilePhone?: Phone;
+    birthDate?: Date;
+    homePhone?: Phone;
+    webSite?: string;
 }
 
 export interface Company {
@@ -11,13 +15,26 @@ export interface Company {
 export interface Professional {
     workdPhone?: Phone;
     jobTitle?: string;
-    company?: Company;
+    company?: Reference;
+    department?: string;
+    companyName?: string;
+}
+
+export interface Communication {
+}
+
+export interface Individual {
+    personal?: Personal;
+    professional?: Professional;
+    communication?: Communication;
 }
 
 export interface Lead extends User {
-    personal?: Personal;
-    professional?: Professional;
+    isPerson?: boolean;
+    individual?: Individual;
     address?: Address;
+    companyName?: string;
+    description?: string;
 }
 
 export interface Account extends Lead {
